83f9dc6217cae51d527b6151678a85eb62d44edb
bhnbhushan <babar.bhushan@gmail.com>
2021-06-10 21:48:28 +0530

    added tests for word guessed table

---
+++ src/components/guessed-word/guessed-word.component.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GuessedWords = ({ guessedWords }) => {
+    let content;
+    if (guessedWords.length === 0) {
+        content = (
+            <span data-test="guess-instructions">Try to guess the secret word!</span>
+        );
+    } else {
+        content = (
+            <div data-test="guessed-words">
+                <h3>Guessed Words</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Guessed word</th>
+                            <th>Matching Letters</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {
+                            guessedWords.map((word, i) => {
+                                return (
+                                    <tr data-test="guessed-word" key={i}>
+                                        <td>{word.guessedWord}</td>
+                                        <td>{word.letterMatchCount}</td>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+    return (
+        <div data-test="component-guessed-words">
+            { content }
+        </div>
+    );
+};
+
+GuessedWords.propTypes = {
+    guessedWords: PropTypes.arrayOf(
+        PropTypes.shape({
+            guessedWord: PropTypes.string.isRequired,
+            letterMatchCount: PropTypes.number.isRequired
+        })
+    ).isRequired
+};
+
+export default GuessedWords;
