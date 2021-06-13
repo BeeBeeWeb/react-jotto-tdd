bc330c818d1f0d0714123cfa26c2e64c01dc193b
bhnbhushan <babar.bhushan@gmail.com>
2021-06-13 13:21:38 +0530

    prop testing for Input component

--- src/components/input/input.test.js
+++ src/components/input/input.test.js
@@ -1,13 +1,22 @@
 import { shallow } from 'enzyme';
-import { findByTestAttr } from '../../test/test.utils';
+import { checkProps, findByTestAttr } from '../../test/test.utils';
 import Input from './input.component';
 
-const setup = () => {
-    return shallow(<Input />);
-}
+const defaultProps = {
+    secretWord: 'train'
+};
+
+const setup = (props = {}) => {
+    const setupProps = { ...defaultProps, ...props };
+    return shallow(<Input {...setupProps} />);
+};
 
 test('renders without errors', () => {
     const wrapper = setup();
     const component = findByTestAttr(wrapper, 'component-input');
     expect(component.length).toBe(1);
+});
+
+test('does not throw error with expected props', () => {
+    checkProps(Input, defaultProps);
 })
