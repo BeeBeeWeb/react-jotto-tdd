bf71f4496bcee2bef0cab2fbb7ac7bce98b60f79
bhnbhushan <babar.bhushan@gmail.com>
2021-06-13 12:58:04 +0530

    added bootstrap styling

--- src/components/guessed-word/guessed-word.component.jsx
+++ src/components/guessed-word/guessed-word.component.jsx
@@ -11,8 +11,8 @@ const GuessedWords = ({ guessedWords }) => {
         content = (
             <div data-test="guessed-words">
                 <h3>Guessed Words</h3>
-                <table>
-                    <thead>
+                <table className="table table-sm">
+                    <thead className="thead-light">
                         <tr>
                             <th>Guessed word</th>
                             <th>Matching Letters</th>
