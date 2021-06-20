9ad636bbed06f168baa495ebcae7feef79622155
bhnbhushan <babar.bhushan@gmail.com>
2021-06-20 14:26:06 +0530

    Add CORRECT_GUESS action and tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
 
+export const actionTypes = {
+    CORRECT_GUESS: 'CORRECT_GUESS'
+}
+
+/**
+ * @function correctGuess
+ * @returns {object} - Action object with type 'CORRECT_GUESS'
+ */
+export function correctGuess() {
+    return { type: actionTypes.CORRECT_GUESS };
+}
+
+/**
+ * @function getSecretWord
+ * @returns {Promise} - Promise that resolves to secret word
+ */
 export const getSecretWord = () => {
     // TODO: write actual action in Redux / Context sections
     return axios.get('http:localhost:3030').then((response) => {
